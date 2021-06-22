57c27593675e3e16e83858eb95a014405980645a
Harles <haryobimo8@gmail.com>
2021-06-22 22:19:11 +0700

    turn on proguard and add certified pinned

--- core/src/main/java/com/expert/core/di/CoreModule.kt
+++ core/src/main/java/com/expert/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.expert.core.data.source.remote.RemoteDataSource
 import com.expert.core.data.source.remote.network.ApiService
 import com.expert.core.domain.repository.IAppRepository
 import com.expert.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,19 +23,26 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<AppDatabase>().appDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             AppDatabase::class.java, "App.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
